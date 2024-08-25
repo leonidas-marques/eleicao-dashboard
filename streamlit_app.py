14e3ef5ec2c549433aa91dfe20405fe074170687
leo.marques <lmarqueslm@outlook.com>
2024-08-25 16:22:12 -0300

    Detalhes adicionados

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import re
 import streamlit as st
 import pandas as pd
 import folium
@@ -19,9 +20,11 @@ def clean_column(df, column_name):
 file_path1 = "./data/eleitorado_local_votacao_2020_geografica.xls"
 file_path2 = "./data/votação_secão_2020 vereador.csv"
 
+
 # Leitura dos arquivos
 df1 = pd.read_excel(file_path1)
 df2 = pd.read_csv(file_path2, sep=';')
+
 
 # Limpeza dos dados
 df1 = clean_column(df1, 'LONGITUDE')
@@ -39,6 +42,7 @@ df1.rename(columns={
 df2_grouped = df2.groupby('NR_LOCAL_VOTACAO', as_index=False).agg({
     'QT_VOTOS': 'sum'
 })
+
 
 # Unir df1 com df2_grouped
 df1 = df1.merge(df2_grouped, left_on='NR_LOCAL_V',
@@ -84,6 +88,7 @@ marker_cluster = MarkerCluster().add_to(m)
 # Adiciona marcadores para o local selecionado
 for idx, row in filtered_df.iterrows():
     popup_content = f"""
+    <strong>Número:</strong> {row['NR_LOCAL_V']}<br>
     <strong>Local:</strong> {row['NM_LOCAL_V']}<br>
     <strong>Tipo:</strong> {row['DS_TIPO_LO']}<br>
     <strong>Total Eleitores:</strong> {row['Quantidade_Eleitores']}<br>
@@ -96,4 +101,38 @@ for idx, row in filtered_df.iterrows():
     ).add_to(marker_cluster)
 
 st_data = st_folium(m, use_container_width=True)
-st.write(st_data.get('last_object_clicked_popup', 'Nenhum objeto clicado'))
+
+# Detecção de clique no mapa
+clicked_object = st_data.get('last_object_clicked_popup', None)
+st.title("Detalhes:")
+if clicked_object:
+    st.write(clicked_object)
+    # Buscar o número do local de votação a partir do objeto clicado
+    print('clicked_object', clicked_object)
+    match = re.search(r'Número: (\d+)', clicked_object)
+    print('match: ', match)
+    if match:
+        nr_local_v = int(match.group(1))
+        print(f'O número do local de votação é: {nr_local_v}')
+        # Filtrar o DataFrame df2 para o local de votação desejado
+
+        df_vereadores = df2[df2['NR_LOCAL_VOTACAO'] == nr_local_v]
+
+        # Agrupar por vereador e somar os votos
+        df_vereadores_grouped = df_vereadores.groupby(['NM_VOTAVEL'], as_index=False).agg({
+            'QT_VOTOS': 'sum'
+        })
+
+        # Ordenar por quantidade de votos, se desejado
+        df_vereadores_grouped = df_vereadores_grouped.sort_values(
+            by='QT_VOTOS', ascending=False)
+
+        # Mostrar os dados
+        st.write('Balanço dos Votos dos Vereadores:')
+        st.dataframe(df_vereadores_grouped, use_container_width=True)
+
+    else:
+        print('Número do local de votação não encontrado.')
+
+else:
+    st.write('Selecione um Local para mais detalhes.')

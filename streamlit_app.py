aaa78b71afc64c25dc8ff45357a3c10f427773d1
leo.marques <lmarqueslm@outlook.com>
2024-08-25 02:34:27 -0300

    Alteração do template

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,148 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+import pydeck as pdk
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
-
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+# Função para limpar e converter dados
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+def clean_column(df, column_name):
+    df[column_name] = df[column_name].astype(str)
+    df[column_name] = df[column_name].str.replace(r'[()]*', '', regex=True)
+    df[column_name] = df[column_name].str.replace(',', '.', regex=False)
+    df[column_name] = pd.to_numeric(df[column_name], errors='coerce')
+    return df
 
-st.header(f'GDP in {to_year}', divider='gray')
+# Função para gerenciar o valor de pitch
 
-''
 
-cols = st.columns(4)
+def manage_pitch():
+    if 'pitch' not in st.session_state:
+        st.session_state.pitch = 45  # Valor inicial do pitch
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+    col1, col2, col3 = st.sidebar.columns([1, 3, 1])
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+    with col1:
+        if st.button('−', key='decrement_pitch'):
+            st.session_state.pitch = max(0, st.session_state.pitch - 1)
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
+    with col2:
+        st.session_state.pitch = st.slider(
+            "Inclinação (Pitch)", min_value=0, max_value=90, value=st.session_state.pitch)
+
+    with col3:
+        if st.button('＋', key='increment_pitch'):
+            st.session_state.pitch = min(90, st.session_state.pitch + 1)
+
+# Função para gerenciar o valor de zoom
+
+
+def manage_zoom():
+    if 'zoom' not in st.session_state:
+        st.session_state.zoom = 12  # Valor inicial do zoom
+
+    col1, col2, col3 = st.sidebar.columns([1, 3, 1])
+
+    with col1:
+        if st.button('−', key='decrement_zoom'):
+            st.session_state.zoom = max(1, st.session_state.zoom - 1)
+
+    with col2:
+        st.session_state.zoom = st.slider(
+            "Nível de Zoom", min_value=1, max_value=20, value=st.session_state.zoom)
+
+    with col3:
+        if st.button('＋', key='increment_zoom'):
+            st.session_state.zoom = min(20, st.session_state.zoom + 1)
+
+
+# Sidebar para upload do arquivo Excel e configurações do mapa
+st.sidebar.header("Configurações do Mapa")
+uploaded_file = st.sidebar.file_uploader(
+    "Escolha um arquivo Excel", type=["xls", "xlsx"])
+
+st.sidebar.markdown("<hr>", unsafe_allow_html=True)
+
+# Barra de pesquisa para o campo "Local"
+if uploaded_file is not None:
+    try:
+        manage_pitch()
+        manage_zoom()
+        color = st.sidebar.color_picker("Cor dos Marcadores", "#0000FF")
+        df = pd.read_excel(uploaded_file)
+
+        # st.write("Dados Importados:", df)
+
+        required_columns = ["LONGITUDE", "LATITUDE",
+                            "NM_LOCAL_V", "NM_BAIRRO", "QT_ELEITOR"]
+        if all(col in df.columns for col in required_columns):
+            df.rename(columns={
+                "LONGITUDE": "Longitude",
+                "LATITUDE": "Latitude",
+                "NM_LOCAL_V": "Local",
+                "NM_BAIRRO": "Bairro",
+                "QT_ELEITOR": "Eleitores"
+            }, inplace=True)
+
+            df = clean_column(df, 'Longitude')
+            df = clean_column(df, 'Latitude')
+
+            unique_locations = df['Local'].dropna().unique()
+            selected_local = st.sidebar.selectbox("Selecionar Local", options=[
+                                                  ""] + list(unique_locations))
+
+            if selected_local:
+                df = df[df['Local'] == selected_local]
+
+            df.dropna(subset=['Longitude', 'Latitude'], inplace=True)
+
+            if not df.empty:
+                # Criar a camada 3D
+                layer = pdk.Layer(
+                    "ColumnLayer",
+                    data=df,
+                    get_position=["Longitude", "Latitude"],
+                    get_elevation="Eleitores",
+                    radius=10,
+                    elevation_scale=1,
+                    get_fill_color=[
+                        int(color[1:3], 16),
+                        int(color[3:5], 16),
+                        int(color[5:7], 16),
+                        180
+                    ],
+                    pickable=True,
+                    auto_highlight=True,
+                    opacity=0.8
+                )
+
+                view_state = pdk.ViewState(
+                    latitude=df['Latitude'].mean(),
+                    longitude=df['Longitude'].mean(),
+                    zoom=st.session_state.zoom,
+                    pitch=st.session_state.pitch
+                )
+
+                deck_map = pdk.Deck(
+                    layers=[layer],
+                    initial_view_state=view_state,
+                    tooltip={
+                        "html": "<b>Local:</b> {Local}<br><b>Bairro:</b> {Bairro}<br><b>Eleitores:</b> {Eleitores}",
+                        "style": {"color": "white"}
+                    }
+                )
+
+                st.pydeck_chart(deck_map)
+
+                # Adicionar gráfico de barras de votos por bairro
+                bairro_counts = df.groupby(
+                    'Bairro')['Eleitores'].sum().reset_index()
+                st.write("Quantidade de Votos por Bairro:")
+                st.bar_chart(bairro_counts.set_index('Bairro'))
+            else:
+                st.write("Nenhum dado encontrado para o local selecionado.")
         else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+            st.error("O arquivo Excel não contém todas as colunas necessárias.")
+    except Exception as e:
+        st.error(f"Ocorreu um erro ao processar o arquivo: {e}")
+else:
+    st.info("Faça o upload de um arquivo Excel para começar.")

b95c0a89c90cbf4596320b2d4ad276c7924d0856
leo.marques <lmarqueslm@outlook.com>
2024-08-27 17:52:24 -0300

    pie chart

--- recursos/vereadores.py
+++ recursos/vereadores.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 def load_vereadores(selected_vereador, df1_grouped):
     if selected_vereador != 'Todos os Vereadores':
-        
+
         df_vereador_bairro = df1_grouped.groupby('NM_BAIRRO').agg({
             selected_vereador: 'sum'
         }).reset_index()
@@ -13,10 +13,15 @@ def load_vereadores(selected_vereador, df1_grouped):
         df_vereador_bairro.rename(
             columns={selected_vereador: 'Votos'}, inplace=True)
 
+        # Filtrar bairros com votos > 0
+        df_vereador_bairro = df_vereador_bairro[df_vereador_bairro['Votos'] > 0]
+
         df_vereador_bairro = df_vereador_bairro.sort_values(
             by='Votos', ascending=True)
 
-        fig = px.bar(df_vereador_bairro, y='NM_BAIRRO', x='Votos', title=f'Quantidade de Votos por Bairro para {selected_vereador}',
+        fig = px.pie(df_vereador_bairro, names='NM_BAIRRO', values='Votos',
+                     title=f'Quantidade de Votos por Bairro para {selected_vereador}',
                      labels={'NM_BAIRRO': 'Bairro', 'Votos': 'Quantidade de Votos'})
+        fig.update_traces(textinfo='value')
 
         st.plotly_chart(fig, use_container_width=True)
